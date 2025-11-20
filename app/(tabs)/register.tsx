d4886f2e799d9e79ce31bd88d525ce24af4c9e9d
santivgo <requiemsantiago@gmail.com>
2025-11-20 15:00:37 -0300

    edit profile && login

--- app/(tabs)/register.tsx
+++ app/(tabs)/register.tsx
@@ -19,6 +19,8 @@ interface ValidationResult {
 }
 
 export default function RegisterScreen() {
+  
+  const [actualError, setActualError] = useState('');
   const [user, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [passwordCheck, setPasswordCheck] = useState('');
@@ -63,6 +65,7 @@ export default function RegisterScreen() {
 
   useFocusEffect(
     useCallback(() => {
+
       setUsername('');
       setPassword('');
       setPasswordCheck('');
@@ -229,13 +232,19 @@ export default function RegisterScreen() {
       return;
     }
 
-    try {
-      await registerUser({ "name": user, "username": userName, "password": password, "cpf": cpf, "state": state as string, "city": city as string });
-      await loginUser({ "username": userName, "password": password });
-      router.push('/profile');
-    } catch (err) {
-      console.error('Erro no registro ou login:', err);
-      Alert.alert('Erro', 'Falha no registro ou login. Tente novamente.');
+     const response = await registerUser({ "name": user, "username": userName, "password": password, "cpf": cpf, "state": state, "city": city })
+
+    if (response.success){
+      try {
+        router.push('/');
+      } catch (err) {
+        console.log('Erro em redirecionar a página:', err);
+      }
+    }
+    else {
+      const firstKey = Object.keys(response.data)[0];
+      const firstMessage = response.data[firstKey][0];
+      setActualError(firstMessage)
     }
   };
 
