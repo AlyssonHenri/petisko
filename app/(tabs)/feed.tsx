7fbf4dae6502840e1052cce3d613da57e88833c7
santivgo <requiemsantiago@gmail.com>
2025-12-06 14:30:34 -0300

    WIP: Feed

--- app/(tabs)/feed.tsx
+++ app/(tabs)/feed.tsx
@@ -1,16 +1,16 @@
 import Colors from "@/constants/Colors";
 import { Keyboard, KeyboardAvoidingView, Pressable, ScrollView } from "react-native";
-import { View } from "react-native";
 import ToastManager from "toastify-react-native/components/ToastManager";
+import TinderCard from '../../animation/tinderCard'
 
 export default function FeedScreen(){
     return (
         <KeyboardAvoidingView style={{flex: 1}}>
-              <ScrollView style={{backgroundColor: Colors.creme}}>
-              <Pressable onPress={() => Keyboard.dismiss()} style={{ flex: 1 }}>
-                oi
-        </Pressable>
-        </ScrollView>
+            <ScrollView style={{backgroundColor: Colors.creme}}>
+                <Pressable onPress={() => Keyboard.dismiss()} style={{ flex: 1 }}>
+                        <TinderCard></TinderCard>
+                </Pressable>
+            </ScrollView>
         <ToastManager />
         </KeyboardAvoidingView>
 
---
+++ interfaces/cardPet.ts
@@ -0,0 +1,8 @@
+export interface CardPet {
+    id: number,
+    name: string,
+    age: number,
+    img: string,
+    raca: string,
+    sex: string,
+}
